89e12fcbf698fd1d9d6494f0a4791788e35e9fd6
PiotrVision <pgarbacz84@gmail.com>
2024-12-28 16:39:04 +0100

    nerd game refactoring
    added function "update_task"

--- pico_nerd_game.py
+++ pico_nerd_game.py
@@ -27,6 +27,17 @@ def test_answer(num, bits):
         return True
     else:
         return False
+    
+def update_task(number, total):
+    
+    lcd.clear()
+    lcd.move_to(0,0)
+    line1 = "Dec Number:{0}".format(number)
+    lcd.putstr(line1)
+    lcd.move_to(0,1)
+    line2 = "Your score:{0}".format(total)
+    lcd.putstr(line2)
+    return cn
 
 lcd.putstr('Hello!')
 utime.sleep_ms(2000)
@@ -37,26 +48,15 @@ utime.sleep_ms(2000)#clear screen needs a long delay
 score = 0
 b_next_task = False
 cn = get_new_number()
-lcd.clear()
-lcd.move_to(0,0)
-line1 = "Digit Number:{0}".format(cn)
-lcd.putstr(line1)
-lcd.move_to(0,1)
-line2 = "Your score:{0}".format(score)
-lcd.putstr(line2)
+update_task(cn, score)
+
 
 while True:
     utime.sleep_ms(20)
     if b_next_task:
         score+=1
         cn = get_new_number()
-        lcd.clear()
-        lcd.move_to(0,0)
-        line1 = "Digit Number:{0}".format(cn)
-        lcd.putstr(line1)
-        lcd.move_to(0,1)
-        line2 = "Your score:{0}".format(score)
-        lcd.putstr(line2)
+        update_task(cn, score)
         b_next_task = False
     for i in range(8):
         if Pin(i, Pin.IN, Pin.PULL_DOWN).value():
